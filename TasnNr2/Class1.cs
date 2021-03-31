066cfa98aea537dae9b4e06473a78fc81e139f66
Slownik1 <76266435+Slownik1@users.noreply.github.com>
2021-03-31 18:47:28 +0200

    Add files via upload

---
+++ TasnNr2/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TasnNr2
+{
+    public static class Class1
+    {
+
+        static String text;
+
+        public static char Convert(){
+
+            text = Console.ReadLine();
+
+            for(int i=0; i< text.Length; i++)
+            {
+                if (Char.IsLower(text[i]))
+                    Console.Write(Char.ToUpper(text[i]));
+                else
+                    Console.Write(Char.ToLower(text[i]));
+            }
+
+            return '@';
+
+            }
+
+    }
+}
---
+++ TasnNr2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TasnNr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.Convert();
+        }
+    }
+}
